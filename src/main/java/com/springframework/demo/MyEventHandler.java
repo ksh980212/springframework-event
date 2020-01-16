b2af25e55751e556cfaaf48766a864e99d47ff6b
ksh980212 <ksh980212@gmail.com>
2020-01-17 00:14:02 +0900

    #2 ContextRefreshedEvent & ContextClosedEvent

--- src/main/java/com/springframework/demo/MyEventHandler.java
+++ src/main/java/com/springframework/demo/MyEventHandler.java
@@ -1,5 +1,7 @@
 package com.springframework.demo;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -14,5 +16,19 @@ public class MyEventHandler {
     @Async
     public void handle(MyEvent event) {
         System.out.println("이벤트 받았다. 데이터는" + event.getData());
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent event) {
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
     }
 }
